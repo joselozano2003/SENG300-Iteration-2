8f66a5671b1ad09453a58541fc656ad88f7841c7
Swaguy14256 <Swaguy14256@users.noreply.github.com>
2023-03-27 15:52:03 -0600

    Fixing up the bag.

--- com.autovend.software/src/com/autovend/software/AddOrRemoveBags.java
+++ com.autovend.software/src/com/autovend/software/AddOrRemoveBags.java
@@ -1,5 +1,6 @@
 package com.autovend.software;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 import com.autovend.devices.*;
@@ -9,21 +10,69 @@ public class AddOrRemoveBags {
 	public ArrayList<Bag> bags;
 	public int ownbags;
 	public int storebags;
+	public int bagsremaining;
+	private boolean purchased;
+	public BigDecimal price = new BigDecimal("0.25");
+	public BigDecimal weight = new BigDecimal("0.050");
 	
 	public AddOrRemoveBags() {
 		
 	}
 	
-	public void purchasebag() {
-		
-	}
-	public void cancelbag() {
-		
+	public void purchasebag() throws SimulationException {
+		if (bagsremaining == 0) {
+			throw new SimulationException("We are out of bags. Get some more from the attendant bub.");
+		}
+		else {
+			bagsremaining--;
+			//bags.add(new Bag(true));
+			if (storebags == Integer.MAX_VALUE) {
+				throw new SimulationException("what - how did you even buy that many bags...?");
+			}
+			storebags++;
+		}
 	}
 	public void addownbag() {
+		if (ownbags == Integer.MAX_VALUE) {
+			throw new SimulationException("...why do you even have this many bags in the first place?");
+		}
+	}
+	public void removebag(int bagnumber) throws SimulationException {
+		try {
+			Bag tempbag = bags.get(bagnumber);
+			if (true) {
+				storebags--;
+				if (tempbag.baggage.isEmpty()) {
+					bags.remove(bagnumber);
+				}
+				else {
+					throw new SimulationException("Empty this bag first. You think these are free?");
+				}
+			}
+			else {
+				ownbags--;
+				if (tempbag.baggage.isEmpty()) {
+					bags.remove(bagnumber);
+				}
+				else {
+					throw new SimulationException("Empty this bag first. Or do you plan on returning what is in this bag?");
+				}
+			}
+		}
+		catch (Exception e) {
+			throw new SimulationException("What are you even trying to remove? Air?");
+		}
+	}
+	public void addtobag(Bag bag) {
 		
 	}
-	public void removebag() {
+	public void removefrombag(Bag bag) {
+		
+	}
+	public void monitorbags() {
+		
+	}
+	public void emptybag(Bag bag) {
 		
 	}
 }
--- com.autovend.software/src/com/autovend/software/Bag.java
+++ com.autovend.software/src/com/autovend/software/Bag.java
@@ -1,10 +1,18 @@
 package com.autovend.software;
 
-public class Bag {
-	private boolean ownbag;
-	
-	public Bag(boolean nonstorebag) {
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import com.autovend.Barcode;
+import com.autovend.products.BarcodedProduct;
+
+public class Bag extends BarcodedProduct {
+	public Bag(Barcode barcode, String description, BigDecimal price, double expectedWeight) {
+		super(barcode, description, price, expectedWeight);
 		
 	}
+
+	public ArrayList<PurchasedItems> baggage;
+	private BigDecimal capacity;
 
 }
