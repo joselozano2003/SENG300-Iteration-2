cdb9c1ffdd0f19fe7eee2c5dcc204b30585eda8f
Swaguy14256 <Swaguy14256@users.noreply.github.com>
2023-03-27 11:26:29 -0600

    Baggage progress.

---
+++ com.autovend.software/src/com/autovend/software/AddOrRemoveBags.java
@@ -0,0 +1,29 @@
+package com.autovend.software;
+
+import java.util.ArrayList;
+
+import com.autovend.devices.*;
+import com.autovend.products.BarcodedProduct;
+
+public class AddOrRemoveBags {
+	public ArrayList<Bag> bags;
+	public int ownbags;
+	public int storebags;
+	
+	public AddOrRemoveBags() {
+		
+	}
+	
+	public void purchasebag() {
+		
+	}
+	public void cancelbag() {
+		
+	}
+	public void addownbag() {
+		
+	}
+	public void removebag() {
+		
+	}
+}
