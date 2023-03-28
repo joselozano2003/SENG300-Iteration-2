228279170e7250e583082c98e85b0af87f484a02
JoseLozano2003 <josecamilo.lozanocet@ucalgary.ca>
2023-03-27 19:51:26 -0600

    Fixed class, should now work. Added the package specifications, renamed the misused scanner and fixed the logic to see if the inout is only integers

--- com.autovend.software/src/com/autovend/software/Membership.java
+++ com.autovend.software/src/com/autovend/software/Membership.java
@@ -1,5 +1,6 @@
-import java.util.Scanner;
+package com.autovend.software;
 
+import java.util.Scanner;
 
 
 public class Membership {
@@ -9,8 +10,8 @@ public class Membership {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Do you wish to provide your Membership Number? (Y/N) ")
-        String response = sc.nextLine();
+        System.out.println("Do you wish to provide your Membership Number? (Y/N) ");
+        String response = input.nextLine();
 
         if (response.equalsIgnoreCase("Y")){
         	 // Display virtual keyboard
@@ -31,18 +32,21 @@ public class Membership {
         }
         //prompts the customer to continue with the self check out if they do not wish to provide Membership number
         else {
-        	System.out.println("Continue with the Self-CheckOut. ")
+        	System.out.println("Continue with the Self-CheckOut. ");
         }
     }
     //Membership number needs to be a string of length 10 and should only have digits from 0 to 9
     //this is the criteria for membership number to be considered valid
     public static void isValid(String membershipNumber) throws Exception {
     	if (membershipNumber.length() != 10) {
-    		throw new Exception("Membership Number should be of 10 digits long")
+    		throw new Exception("Membership Number should be of 10 digits long");
     	}
-    	if(c < '0' || c > '9') {
-    		throw new Exception("Membership number should only contain digits from 0 to 9.")
-    	}
+        for (char c : membershipNumber.toCharArray()) {
+            if(c < '0' || c > '9') {
+                throw new Exception("Membership number should only contain digits from 0 to 9.");
+            }
+        }
+
     }
     
   }
